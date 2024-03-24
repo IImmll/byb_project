c18328c360be2a6f6f4f2186562943f4fb1ccff2
JV-III <jv@JV-IIIs-MacBook-Pro.local>
2024-03-24 16:23:17 +0000

    hello

---
+++ hello_world.py
@@ -0,0 +1,22 @@
+
+# Pseudocode comments:
+# 1. Prompt the user to enter their name
+# 2. Print the user's name
+# 3. Prompt the user to enter their age
+# 4. Print the user's age
+# 5. Print "Hello World!" on a new line
+
+# Prompting the user for their name
+name = input("Enter your name: ")
+
+# Printing the user's name
+print("Your name is:", name)
+
+# Prompting the user for their age
+age = input("Enter your age: ")
+
+# Printing the user's age
+print("You are", age, "years old.")
+
+# Printing "Hello World!" on a new line
+print("Hello World!")
